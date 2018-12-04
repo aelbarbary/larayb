2535d1b5b94d449a9cd742e84eb331b638e426c7
Abdelrahman Elbarbary <abdelrahman.elbarbary@gmail.com>
2018-12-04 13:52:32 -0800

    test

--- webapp/larayb/src/App.js
+++ webapp/larayb/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            Learn React 4
           </a>
         </header>
       </div>
