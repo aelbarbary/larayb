95fa8b1be85967279722d2df93367024dced9d04
Abdelrahman Elbarbary <abdelrahman.elbarbary@gmail.com>
2018-12-18 14:52:31 -0800

    event card

---
+++ webapp/larayb/src/components/Event/index.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
+const styles = theme => ({
+  card: {
+    maxWidth: 400,
+    textAlign: 'center',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    marginLeft: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: -8,
+    },
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+});
+
+class Event extends Component {
+  state = { expanded: false };
+
+  render() {
+    const { classes } = this.props;
+    const { event } = this.props;
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={event.title}
+          subheader={event.datetime}
+        />
+        <CardMedia
+          className={classes.media}
+          image={event.image}
+          title="Paella dish"
+        />
+        <CardContent>
+          <Typography component="p">
+            {event.description}
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.actions} disableActionSpacing>
+          <IconButton aria-label="Add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="Share">
+            <ShareIcon />
+          </IconButton>
+
+        </CardActions>
+
+      </Card>
+    );
+  }
+}
+
+Event.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Event);
--- webapp/larayb/src/components/Offers/index.js
+++ webapp/larayb/src/components/Offers/index.js
@@ -3,19 +3,18 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import InfiniteScroll from 'react-infinite-scroller';
-import qwest from 'qwest';
+// import qwest from 'qwest';
+import firebase from '../../lib/firebase.js';
+import Event from '../Event/index';
 
-const api = {
-    baseUrl: 'https://api.soundcloud.com',
-    client_id: 'caf73ef1e709f839664ab82bef40fa96'
-};
+const firestore = firebase.firestore();
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
     marginTop: 20
   },
-  card:{
+  event:{
     textAlign: 'center'
   }
 });
@@ -26,51 +25,40 @@ class Offers extends Component {
        super(props);
 
        this.state = {
-           tracks: [],
+           events: [],
            hasMoreItems: true,
            nextHref: null
        };
+
+      this.loadItems = this.loadItems.bind(this);
    }
 
   loadItems(page) {
         var self = this;
 
-        var url = api.baseUrl + '/users/8665091/favorites';
-        if(this.state.nextHref) {
-            url = this.state.nextHref;
-        }
 
-        qwest.get(url, {
-                client_id: api.client_id,
-                linked_partitioning: 1,
-                page_size: 10
-            }, {
-                cache: true
-            })
-            .then(function(xhr, resp) {
-                if(resp) {
-                    var tracks = self.state.tracks;
-                    resp.collection.map((track) => {
-                        if(track.artwork_url == null) {
-                            track.artwork_url = track.user.avatar_url;
-                        }
-
-                        tracks.push(track);
-                        return ""
-                    });
-
-                    if(resp.next_href) {
-                        self.setState({
-                            tracks: tracks,
-                            nextHref: resp.next_href
-                        });
-                    } else {
-                        self.setState({
-                            hasMoreItems: false
-                        });
-                    }
-                }
+        var events = self.state.events;
+        firestore.collection("offers")
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+                events.push(doc.data());
             });
+        })
+        .then(()=>{
+          self.setState({
+                 events: events,
+                 hasMoreItems: false
+              });
+        })
+        .catch(function(error) {
+            console.log("Error getting documents: ", error);
+        });
+
+
+
     }
 
   render() {
@@ -79,13 +67,11 @@ class Offers extends Component {
     const loader = <div className="loader">Loading ...</div>;
 
     var items = [];
-    this.state.tracks.map((track, i) => {
+    this.state.events.map((track, i) => {
+
         items.push(
-            <Grid item xs={4} className={classes.card}>
-                <a href={track.permalink_url} >
-                    <img src={track.artwork_url} width="150" height="150" alt="" />
-                    <p className="title">{track.title}</p>
-                </a>
+            <Grid item xs={4}>
+              <Event event={track} className={classes.event}></Event>
             </Grid>
         );
         return ""
@@ -99,7 +85,7 @@ class Offers extends Component {
             hasMore={this.state.hasMoreItems}
             loader={loader}>
             <Grid container spacing={24}>
-                {items}
+              {items}
             </Grid>
         </InfiniteScroll>
       </div>
